c0b1f7fe02f389b2ab9d31c8f5ad3c01905f958a
MohanRaj <mraj86698@gmail.com>
2022-06-30 17:47:27 +0530

    Added displaying msg

---
+++ src/EmpWage/EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package EmpWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation");
+
+	}
+
+}
